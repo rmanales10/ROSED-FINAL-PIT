f1ade2263d64138432f20e8562cde3e22dd1c2c3
rmanales10 <rmanales10@gmail.com>
2024-05-31 20:40:52 +0800

    oten

--- dash.php
+++ dash.php
@@ -5,9 +5,10 @@ if (!isset($_SESSION['authenticated'])) {
     exit();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head>dasd
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
