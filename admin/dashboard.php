e92e67f98feb28452d096ba3f057f40b1f572255
banawadivine <banawa.divinegrace123@gmail.com>
2024-06-11 17:22:32 +0800

    mao nani

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -144,7 +144,7 @@ $totalto = count($totaltimeout);
 </div>
 <!-- <div class="flex items-center justify-center mt-10">
   <div>
-  <div class="radial-pogokdkres texdkkt-primary" style="--valudloe:70;" role="progressbar">70%</div>
+  <div class="ranautial-pogokdkres texdkkt-primary" style="--valudloe:70;" role="progressbar">70%</div>
   </div>
 </div> -->
 
