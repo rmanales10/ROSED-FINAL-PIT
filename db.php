ada6e846c4b424b9f5772220ca8979b880405def
banawadivine <banawa.divinegrace123@gmail.com>
2024-06-11 17:26:08 +0800

    im not

--- db.php
+++ db.php
@@ -19,7 +19,7 @@ class Database {
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
         } else {
-            //echo "Connected successfullys";
+            //echo "Connected successfully";
         }
     }
 
