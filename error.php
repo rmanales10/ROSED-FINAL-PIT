c0962f36d979a2d2c761bca9bedf6d710d949efa
Bwiseth <162430021+Bwiseth@users.noreply.github.com>
2024-06-13 06:46:52 +0800

    Update error.php

--- error.php
+++ error.php
@@ -121,7 +121,7 @@ body {
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--magic WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
+	<!--[if lt IE 9]> Remind lang ko nga gwapo si Rolan Manales :)) 
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
